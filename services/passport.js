fdcc0ec33c0f0ed6d82f89b1c0bbd27d7efff2a6
vanpeta <carlosallendesanz@gmail.com>
2017-08-04 11:03:40 -0700

    using mongoose to save new users only.

--- services/passport.js
+++ services/passport.js
@@ -12,7 +12,17 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			new User({ googleId: profile.id }).save();
+			User.findOne({ googleId: profile.id })
+				.then((existingUser)=> {
+					if (existingUser) {
+						// User is already in the db
+						done(null, existingUser);
+					} else {
+						// New User signed up
+						new User({ googleId: profile.id }).save()
+							.then(user => done(null, user));
+					}
+				})
 		}
 	)
 );
