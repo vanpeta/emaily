9c1ffeb4997accdae5c450a4e4c34c92de2b3841
vanpeta <carlosallendesanz@gmail.com>
2017-08-04 12:20:11 -0700

    adding logout funtionality

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,14 @@ module.exports = (app) => {
 	app.get(
 		'/auth/google/callback', passport.authenticate('google')
 	);
+	
+	app.get('/api/logout', (req, res) => {
+		console.log('logout')
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
 }
