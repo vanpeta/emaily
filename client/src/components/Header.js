377989adc9924d51a527122535b43456dd5d575d
vanpeta <carlosallendesanz@gmail.com>
2017-08-07 14:04:00 -0700

    Adding the action creator to the component and render content based on the state changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
+			default:
+				return <li><a>Log Out</a></li>;
+
+		}
+	}
+
 	render() {
 		return (
 			<nav>
@@ -9,9 +26,7 @@ class Header extends Component {
 						Emaily
 					</a>
 					<ul className="right">
-						<li>
-							<a>Login With Google</a>
-						</li>
+						{this.renderContent()}
 					</ul>
 				</div>
 			</nav>
@@ -19,4 +34,8 @@ class Header extends Component {
 	}
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
