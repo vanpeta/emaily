e7b1c67879173697369668ab8aa7b5c533ab7632
vanpeta <carlosallendesanz@gmail.com>
2017-08-07 16:52:37 -0700

    adding stripecheckout to header and wiring it up

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
 	renderContent() {
@@ -9,12 +10,12 @@ class Header extends Component {
 				return;
 			case false:
 				return (
-					<li>
-						<a href="/auth/google">Login With Google</a>
-					</li>
+					<li><a href="/auth/google">Login With Google</a></li>
 				);
 			default:
-				return <li><a href="/api/logout">Log Out</a></li>;
+				return [
+					<li key="1"><Payments /></li>,
+					<li key="2"><a href="/api/logout">Log Out</a></li>]
 		}
 	}
 
